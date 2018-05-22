3e3a752316eec6259eb86fe279d496ca395a16ea
ajboxjr <ajboxjr@gmail.com>
2018-05-22 19:33:00 -0400

    add isValidPassword utility

--- controllers/user.js
+++ controllers/user.js
@@ -8,11 +8,13 @@ const router = express.Router();
 const User = require('../models/user');
 const Dream = require('../models/dream')
 
+var { isValidPassword } = require('../utils/utils')
 
 router.delete('/user/delete', (req, res) => {
   //Verfiy password
   //for each dream in user delete from db
   var oldpwd = req.body.oldpassword;
+  if( oldpwd ){
     User.findById({_id: req.user._id}, 'username password').populate('dreams').exec((err, user) => {
       if (user){
         //Delete user
@@ -23,18 +25,22 @@ router.delete('/user/delete', (req, res) => {
               //Optional logging for whether dream is found
               Dream.findByIdAndRemove({ _id: dreamId })
               user.remove()
-          })
-          res.status(200).json({message:'user and dreams deleted successfuly', success: true})
-        }
-        else{
-          res.status(400).json({message:'invalid credentials', success: false, err:"invalid password"});
-        }
-      })
-    }
-    else {
-      res.status(400).json({message:"User not found", success:false, err:err})
-    }
-  })
+            })
+            res.status(200).json({ err:['user and dreams deleted successfuly'], success: true })
+          }
+          else{
+            res.status(400).json({ err:['invalid credentials'], success: false });
+          }
+        })
+      }
+      else {
+        res.status(400).json({ message:"User not found", success:false, err:err })
+      }
+    })
+  }
+  else {
+    res.status(400).json({ success:false, err: ["missing field"] })
+  }
 })
 
 router.post('/user/change-password', (req,res) => {
