fc9f98d13710b19321c57fec4103f05c717ad3f8
ajboxjr <ajboxjr@gmail.com>
2018-11-10 11:36:15 -0600

    remove garbage token add seralized error response

--- handlers/token.js
+++ handlers/token.js
@@ -1,60 +1,27 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
-
-/*
-  // TODO: Store expired token to mongodb to
-  to prevent reuse
-*/
-
-const garbageToken = async (token) => {
-  return new Promise((resolve, reject) => {
-    //// TODO: Store token to used database
-    User.findOne({token: "token"}, function(err, user){
-      console.log(user);
-      if(!err){
-        user.token = undefined;
-        console.log('successfuly removed token');
-        user.save()
-        resolve()
-      }
-      else {
-        console.log('error');
-        reject()
-      }
-    })
-  })
-}
+const {errorResponse} = require('./jsonResponse/jsonResponse')
 
 /*
   Handle incoming tokens by storing to request
   or invalidating token
 */
-exports.requireValidToken = function(req,res,next) {
+
+exports.requireValidToken = function(req, res, next) {
   //Get token
   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  console.log(token);
-  if (token){ //Decode nToken and verify
-   //Verify token user/ // TODO: handle expired token
-   jwt.verify(token, process.env.SECRET , function(err, decodedToken) {
-     if(decodedToken){
-       console.log(decodedToken);
-       req.user = decodedToken;
-       next();
-     }
-     else {
-       //send invalid token and delete token fuction.
-       garbageToken().then(()=> {
-         console.log('sucess duplicate');
-       }).catch(()=> {
-         console.log('not success');
-       })
-       console.log('bob');
-       res.status(402).json({ tokenErr: 'Inavlid or expired token'})
+  if(!token){
+    return res.status(401).json(errorResponse("Please login to obtain access to this resource","1352",'self'))
+  }
+  //Decode nToken and verify
+  //Verify token user/  TODO: handle expired token
+  jwt.verify(token, process.env.SECRET, function(err, decodedToken) {
+    if (!decodedToken) {
+      return res.status(401).json(errorResponse("Session expired, please re-login","1355" ,{self:"/api" + req.url}));
     }
-    })
-  }
-  else {
-    res.status(403).json({ message: 'This path requires a token'});
-  }
+    console.log('found token');
+    req.user = decodedToken;
+    return next();
+  })
 }
